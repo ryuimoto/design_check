0dcc2a37a0210bc606e3b7f7eb36a37f9a6b2b2a
maca <narrow617@gmail.com>
2022-02-27 15:34:09 +0900

    add template user dashboard

--- resources/views/user/dashboard/top.blade.php
+++ resources/views/user/dashboard/top.blade.php
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>これからダッシュボード作ってくよん</h1>
-    <a href="{{ route('user.logout') }}">ログアウト</a>
-</body>
-</html>
+@extends('templates.user.dashboard')
